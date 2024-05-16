3de776a4e8bea7cc81026b54c764306194c3dc03
Mario Contreras <mariocc29@gmail.com>
2024-05-16 18:42:54 -0400

    removed redux implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { store } from '@/state/store'
-import { Provider } from 'react-redux'
 
 import { router } from '@/router/AppRouter'
 import '@/assets/fontello/css/fontello.css'
@@ -10,8 +8,6 @@ import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- src/state/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import statsReducer from "./stats/statsSlice"
-
-export const store = configureStore({
-  reducer: {
-    stats: statsReducer,
-  }
-})
